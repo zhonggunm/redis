d6ab9aabb8fb8bcce8b3f8dd5a246f79324275a9
zhonggunm@hotmail.com <zhonggunm@hotmail.com>
2020-03-10 16:11:37 -0600

    init

---
+++ src/main/java/com/zhong/myapp/redis_service/RedisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zhong.myapp.redis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisServiceApplication.class, args);
+	}
+
+}
